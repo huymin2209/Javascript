db2334aebf19cf28b4be0945d3ddfcb82478d1cf
HUY Pham Quang (VTI.GA) <huy.phamquang@vti.vn>
2021-03-25 17:15:48 +0700

    javascript

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+
+
+// khai bao bien
+var fullName = 'Pham Quang Huy 22'
+
+
+
+// goi ham thong bao
+alert(fullName)
+alert('HI Pham Quang Huy')
+
+
+/*
+Một số hàm Built-in
+1. Alert
+2. Console
+3. Confirm
+4. Prompt
+5. Set timeout
+6. Set interval 
+ */
