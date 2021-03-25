1751f3a072250118b193a9e2aba9bf2c6265b5d0
HUY Pham Quang (VTI.GA) <huy.phamquang@vti.vn>
2021-03-25 17:40:27 +0700

    hi

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-
-
-
-
-// khai bao bien
-var fullName = 'Pham Quang Huy 22'
-
-
-
-// goi ham thong bao
-alert(fullName)
-alert('HI Pham Quang Huy')
-
-
 /*
 Một số hàm Built-in
 1. Alert
@@ -21,3 +7,31 @@ Một số hàm Built-in
 5. Set timeout
 6. Set interval 
  */
+
+
+
+// khai bao bien
+// var fullName = 'Pham Quang Huy 22'
+
+
+// goi ham thong bao
+// alert(fullName)
+// alert('HI Pham Quang Huy')
+
+
+// console.log(fullName)
+
+// confirm('xac nhan')
+
+// prompt('xac nhan')
+
+
+/*
+setTimeout(function() {
+    alert('Thong bao')
+}, 1000)
+*/
+
+setInterval(function() {
+    console.log('day la thong bao' + Math.random())
+}, 1000)
